29086ccc44116ce6008c8f3754da703d90f13d5a
Alec321 <aharrison@sourcealllies.com>
2018-11-05 13:20:04 -0600

    WiP

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -21,6 +21,11 @@ aws s3api wait bucket-exists --bucket $bucketName
 echo "Putting the zipped code into the S3 bucket..."
 aws s3api put-object --bucket $bucketName --key artifact.zip --body artifact.zip
 
+echo "Hosted Zone"
+host_zone=$(aws route53 list-hosted-zones-by-name --query 'HostedZones[?Name == `'sourceallies.com.'`].Id')
+IFS="/" read -r -a host_zone <<< "${host_zone}"
+host_zone_name=${host_zone[2]}
+
 echo "Creating the lambdas..."
 aws cloudformation deploy --stack-name $STACK_NAME \
     --template-file deploy/cloudformation.template.JSON \
@@ -34,7 +39,10 @@ aws cloudformation deploy --stack-name $STACK_NAME \
         ClientKey=$CLIENT_KEY \
         ClientSecret=$CLIENT_SECRET \
         BucketName=$bucketName \
+        StackName=$STACK_NAME\
         ApiUrl=$API_URL \
+        HostedZone=$host_zone_name \
+        DomainName="${bamboo_domain_name}" \
         OAuthCustomHeaders=$OAUTH_CUSTOM_HEADERS \
         AuthorizeCallbackUri=$AUTHORIZE_CALLBACK_URI \
     --no-fail-on-empty-changeset \
