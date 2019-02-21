02ccb73dd29851947afe918da1b07a79d5fcdf63
Shane Drafahl <drafahls@gmail.com>
2019-02-21 10:53:19 -0600

    Fixed IAM role

--- deploy/bamboo/deleteDomain.sh
+++ deploy/bamboo/deleteDomain.sh
@@ -7,8 +7,12 @@ DEPLOY_ENVIRONMENT=$bamboo_deploy_environment
 # Extract environment variables
 DOMAIN_NAME=$bamboo_DOMAIN_NAME
 
+# Look up the IAM admin role ARN for the environment we are deploying into
+ADMIN_ARN="$(printenv bamboo_SAI_${DEPLOY_ENVIRONMENT}_ADMIN_ARN )"
+
 # Write environment variables to .env file
 echo "DOMAIN_NAME=${DOMAIN_NAME}" > .env
+echo "ADMIN_ARN=${ADMIN_ARN}" >> .env
 
 # Run deleteDomain script
 bash deploy/deleteDomain.sh
