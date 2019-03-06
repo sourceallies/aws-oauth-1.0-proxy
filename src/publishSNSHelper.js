64d3ddf52f2f6aaf454661f54f4d88a3ac29cdfd
Shane Drafahl <drafahls@gmail.com>
2019-03-06 16:08:25 -0600

    Got tests to pass, changed the SNS helper to take the message and ARN from config for AWS.

--- src/publishSNSHelper.js
+++ src/publishSNSHelper.js
@@ -1,34 +1,17 @@
 const AWS = require('aws-sdk');
+const config = require('../config');
 
-const publishSuccess = async (params) => {
+const publishToSNSSuccess = async (message) => {
+  const params = {
+     Message: message,
+     TopicArn: config.snsSuccessArn
+  };
+
   AWS.config.update({ region: 'us-east-1' });
   const awsSNS = new AWS.SNS({ apiVersion: '2010-03-31' });
-  await awsSNS.publish(params).promise()
+  await awsSNS.publish(params).promise();
 };
 
 module.exports = {
-  publishSuccess,
+  publishToSNSSuccess,
 };
-
-
-// AWS.config.update({ region: 'us-east-1' });
-// // Create publish parameters
-// const params = {
-//   Message: 'MESSAGE_TEXT', /* required */
-//   TopicArn: 'arn:aws:sns:us-east-1:729161019481:aws-auth-success-topic',
-// };
-//
-// // Create promise and SNS service object
-// const publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
-//
-// // Handle promise's fulfilled/rejected states
-// publishTextPromise.then(
-//   (data) => {
-//     console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`);
-//     console.log(`MessageID is ${data.MessageId}`);
-//   },
-// ).catch(
-//   (err) => {
-//     console.error(err, err.stack);
-//   },
-// );
