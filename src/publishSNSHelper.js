e2473344cb3914839784a6530347371ca35533d1
Shane Drafahl <drafahls@gmail.com>
2019-03-06 16:15:46 -0600

    Refactored into helper method#

--- src/publishSNSHelper.js
+++ src/publishSNSHelper.js
@@ -2,15 +2,18 @@ const AWS = require('aws-sdk');
 const config = require('../config');
 
 const publishToSNSSuccess = async (message) => {
+  await publishToSNS(message, config.snsSuccessArn);
+};
+
+const publishToSNS = async (message, arn) => {
   const params = {
      Message: message,
-     TopicArn: config.snsSuccessArn
+     TopicArn: arn
   };
-
   AWS.config.update({ region: 'us-east-1' });
   const awsSNS = new AWS.SNS({ apiVersion: '2010-03-31' });
   await awsSNS.publish(params).promise();
-};
+}
 
 module.exports = {
   publishToSNSSuccess,
