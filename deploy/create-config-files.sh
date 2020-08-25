7b2ddf93e86b1039af95c3d156509a9c914acce0
Neil Bode <nbode@sourceallies.com>
2020-08-25 11:05:48 -0500

    adding quotes around the path search to prevent searching outside of source folder
    Co-authored-by: Erik Rasmussen <erasmussen@sourceallies.com>

--- deploy/create-config-files.sh
+++ deploy/create-config-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in $(find deploy -type f -path *.params.json); do \
+for file in $(find deploy -type f -path "*.params.json"); do \
     envName=$(echo $file | xargs basename | sed "s/.params.json//"); \
     params=$(cat $file); \
     config="{}"; \
