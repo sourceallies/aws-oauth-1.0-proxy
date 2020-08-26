898ab0756273f3306262b1902f8b10ef4ec912eb
Erik Rasmussen <erasmussen@sourceallies.com>
2020-08-26 16:30:27 -0500

    Format file

--- src/SignAndDelete.js
+++ src/SignAndDelete.js
@@ -1,45 +1,45 @@
 const doSignAndDelete = async (
-    linkToOpen,
-    accessToken,
-    accessTokenSecret,
-    optionalAuthorizeCallbackUri
-  ) => {
-    const config = await getConfig();
-    const authorizeCallbackUri = optionalAuthorizeCallbackUri || config.authorizeCallbackUri;
-  
-    const oAuthSession = new OAuth(
-      config.firstLegUri,
-      config.thirdLegUri,
-      config.clientKey,
-      config.clientSecret,
-      config.oAuthVersion,
-      authorizeCallbackUri,
-      config.oAuthSignatureMethod,
-      config.oAuthNonceSize,
-      config.oAuthCustomHeaders,
+  linkToOpen,
+  accessToken,
+  accessTokenSecret,
+  optionalAuthorizeCallbackUri
+) => {
+  const config = await getConfig();
+  const authorizeCallbackUri =
+    optionalAuthorizeCallbackUri || config.authorizeCallbackUri;
+
+  const oAuthSession = new OAuth(
+    config.firstLegUri,
+    config.thirdLegUri,
+    config.clientKey,
+    config.clientSecret,
+    config.oAuthVersion,
+    authorizeCallbackUri,
+    config.oAuthSignatureMethod,
+    config.oAuthNonceSize,
+    config.oAuthCustomHeaders
+  );
+
+  return await new Promise((resolve, reject) => {
+    console.log(oAuthSession);
+    oAuthSession.delete(
+      linkToOpen,
+      accessToken,
+      accessTokenSecret,
+      (error, responseData, result) => {
+        console.log("Delete Result", {
+          error,
+          responseData,
+          result,
+        });
+        if (error) {
+          reject(error);
+        }
+        if (result.statusCode >= 200 && result.statusCode < 300) {
+          resolve(responseData);
+        }
+        resolve(getStatusText(result.statusCode));
+      }
     );
-  
-    return await new Promise((resolve, reject) => {
-      console.log(oAuthSession);
-      oAuthSession.delete(
-        linkToOpen,
-        accessToken,
-        accessTokenSecret,
-        (error, responseData, result) => {
-          console.log('Delete Result', {
-            error,
-            responseData,
-            result,
-          });
-          if (error) {
-            reject(error);
-          }
-          if (result.statusCode >= 200 && result.statusCode < 300) {
-            resolve(responseData);
-          }
-          resolve(getStatusText(result.statusCode));
-        },
-      );
-    });
-  };
-  
+  });
+};
